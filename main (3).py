19caeda39d654fa7ae440b84c390c218d7beea0f
gokuleshwari963 <145656818+gokuleshwari963@users.noreply.github.com>
2023-09-22 11:58:26 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,9 @@
+def linearSearchProduct( productList,target):
+  indices=[]
+  for index, product in enumerate( productList:
+    if product == targetProduct:
+      indicates. append( index)
+      return indices
+      (variable) products:list[str]
+      products =[ ]
+      
